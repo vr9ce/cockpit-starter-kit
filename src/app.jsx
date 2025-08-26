39c630424673a2003f722dadc0377c239e9abdee
谢骐 <shynur@outlook.com>
2025-08-26 16:59:39 +0800

    ;

---
+++ src/app.jsx
@@ -0,0 +1,26 @@
+import React, {useEffect, useState} from "react"
+import {Alert} from "@patternfly/react-core/dist/esm/components/Alert/index.js"
+import {Card, CardBody, CardTitle} from "@patternfly/react-core/dist/esm/components/Card/index.js"
+import cockpit from "cockpit"
+
+import Game from "./components/Game.jsx"
+
+export function Application() {
+    const [hostname, setHostname] = useState(cockpit.gettext("Unknown"))
+
+    useEffect(() => {
+        const hostname = cockpit.file("/etc/hostname")
+        hostname.watch(content => setHostname(content?.trim() ?? ""))
+        return hostname.close
+    }, [])
+
+    return (
+        <Card>
+            <CardTitle>Starter Kit</CardTitle>
+            <CardBody>
+                <Alert variant='info' title={cockpit.format(cockpit.gettext("Running on $0"), hostname)} />
+            </CardBody>
+            <Game />
+        </Card>
+    )
+}
