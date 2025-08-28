648978c2eeef63511244fa4b892c3d0f95f118a7
谢骐 <shynur@outlook.com>
2025-08-28 12:36:01 +0800

    ;

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,71 +1,75 @@
 // @ts-check
 
-import React, {useState, useEffect} from "react"
+import React from "react"
 import cockpit from "cockpit"
 
-const proc_tree_open_db_req = indexedDB.open("shynur", 1)
-
 /**
- * @param {IDBVersionChangeEvent} event
+ * @type {IDBDatabase}
  */
-proc_tree_open_db_req.onupgradeneeded = function(event) {
-    const db = this.result
+const db = await new Promise(
+    (res, rej) => {
+        const open_db_req = indexedDB.open("shynur", 1)
+        /**
+         * @param {IDBVersionChangeEvent} event
+         */
+        open_db_req.onupgradeneeded = function(event) {
+            const db = this.result
+            switch (event.oldVersion) {
+                case 0:
+                    db.createObjectStore("ProcTree", {keyPath: "timestamp"})
+                    break
+                default:
+                    rej(new Error(`不支持的 IndexedDB/shynur 版本: ${event.oldVersion}`))
+            }
+        }
+        open_db_req.onblocked = function() {
+            alert("请关闭其它打开本网页客户端的旧版本标签页, 然后刷新本页面!")
+            rej(new Error)
+        }
+        open_db_req.onerror = function() {rej(this.error)}
+        open_db_req.onsuccess = function() {
+            const db = this.result
+            db.onversionchange = function () {
+                this.close()
+                alert("网页客户端有新版本, 请关闭当前页面 (旧页面)!")
+                rej(new Error)
+            }
 
-    switch (event.oldVersion) {
-        case 0:
-            upgrade_to_1()
-            break
-        case 1:
-            upgrade_to_2()
-            break
+            res(db)
+        }
     }
+)
 
-    function upgrade_to_1() {
-        const store_samples = db.createObjectStore("ProcTree", {keyPath: "timestamp"})
-    }
-
-    function upgrade_to_2() {}
-}
-
-proc_tree_open_db_req.onerror = function() {console.error(this.error)}
-
-proc_tree_open_db_req.onsuccess = function() {
-    const db = this.result
-
-    db.onversionchange = function() {
-        this.close()
-        alert("网页客户端有新版本, 请关闭当前页面 (旧页面)!")
-    }
-
-    // @ts-ignore
-    !function fetchDataPeriodically() {
-        new Promise(res => res(cockpit.spawn([
+!async function fetchDataPeriodically() {
+    const stdout = await cockpit.spawn(
+        [
             "bash", "-c",
             `. ~/.nvm/nvm.sh; node --input-type=module -e "${
-                '`cat /usr/local/bin/psutil.mjs`;'
-                + 'console.log(JSON.stringify({timestamp:Date.now()/1e3,data:await makeProcTree_Linux(1)}))'
+                "`cat /usr/local/bin/psutil.mjs`;"
+                + "console.log(JSON.stringify({timestamp:Date.now()/1e3,data:await makeProcTree_Linux(1)}))"
             }"`
-        ]))).then(
-            res => {
-                const tx = db.transaction("ProcTree", "readwrite")
+        ]
+    )
 
-                const req = tx.objectStore("ProcTree").add(JSON.parse(res))
-                req.onerror = function(event) {console.log(this.error)}
-
-                tx.oncomplete = function() {
-                    console.log("成功从控制器获取到一次 '/proc' 采样并保存到本地客户端")
-                }
+    const tx = db.transaction("ProcTree", "readwrite")
+    await new Promise(
+        (res, rej) => {
+            tx.oncomplete = function () {
+                console.log("成功从控制器获取到一次 '/proc' 采样并保存到本地客户端")
+                res(undefined)
             }
-        ).catch(
-            err => console.error("从控制器获取 '/proc' 采样失败: " + err.message)
-        ).finally(() => setTimeout(fetchDataPeriodically, 5_000))
-    }()
-}
+            tx.onerror = function () {rej(this.error)}
+            tx.objectStore("ProcTree").add(JSON.parse(stdout))
+        }
+    ).catch(
+        err => console.error("从控制器获取 '/proc' 采样失败: " + err.message)
+    )
 
-proc_tree_open_db_req.onblocked = function() {
-    alert("请关闭其它打开本网页客户端的旧版本标签页, 然后刷新本页面!")
-}
+    setTimeout(fetchDataPeriodically, 5_000)
+}()
 
-export default function() {
-    return <pre>1</pre>
+import Panel from "./Panel.jsx"
+
+export default function () {
+    return <Panel procTreeArray={[]} />
 }
--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -1,7 +1,75 @@
 // @ts-check
 
-import React, {useState, useEffect} from "react"
+import React, {useEffect, useState} from "react"
+import {TreeView} from "@patternfly/react-core"
 
-export default function({children}) {
+/**
+ * @typedef {Object} procTree
+ * @prop {string} name
+ * @prop {string} id
+ * @prop {{ "aria-label"?: string, checked?: boolean }} checkProps
+ * @prop {procTree[]} children?
+ * @prop {boolean} defaultExpanded?
+ */
 
+/**
+ *
+ * @param {{procTreeArray: procTree[]}} procTreeArray
+ */
+export default function ({procTreeArray}) {
+    const [checkedItems, setCheckedItems] = useState([])
+    //useEffect(() => {
+    //    console.log("Checked items: ", checkedItems)
+    //}, [checkedItems])
+
+    const onCheck = (event, treeViewItem) => {
+        const checked = event.target.checked
+        const checkedItemTree = procTreeArray.map(opt => Object.assign({}, opt)).filter(item => filterItems(item, treeViewItem))
+        const flatCheckedItems = flattenTree(checkedItemTree)
+        setCheckedItems(prevCheckedItems => (checked ? prevCheckedItems.concat(flatCheckedItems.filter(item => !checkedItems.some(i => i.id === item.id))) : prevCheckedItems.filter(item => !flatCheckedItems.some(i => i.id === item.id))))
+    }
+    const isChecked = dataItem => checkedItems.some(item => item.id === dataItem.id)
+    const areAllDescendantsChecked = dataItem => (dataItem.children ? dataItem.children.every(child => areAllDescendantsChecked(child)) : isChecked(dataItem))
+    const areSomeDescendantsChecked = dataItem => (dataItem.children ? dataItem.children.some(child => areSomeDescendantsChecked(child)) : isChecked(dataItem))
+    const flattenTree = tree => {
+        let result = []
+        tree.forEach(item => {
+            result.push(item)
+            if (item.children) {
+                result = result.concat(flattenTree(item.children))
+            }
+        })
+        return result
+    }
+    const mapTree = item => {
+        const hasCheck = areAllDescendantsChecked(item)
+        if (item.checkProps) {
+            item.checkProps.checked = false
+            if (hasCheck) {
+                item.checkProps.checked = true
+            } else {
+                const hasPartialCheck = areSomeDescendantsChecked(item)
+                if (hasPartialCheck) {
+                    item.checkProps.checked = null
+                }
+            }
+            if (item.children) {
+                return {
+                    ...item,
+                    children: item.children.map(child => mapTree(child))
+                }
+            }
+        }
+        return item
+    }
+    const filterItems = (item, checkedItem) => {
+        if (item.id === checkedItem.id) {
+            return true
+        }
+        if (item.children) {
+            return (item.children = item.children.map(opt => Object.assign({}, opt)).filter(child => filterItems(child, checkedItem))).length > 0
+        }
+    }
+    const mapped = procTreeArray.map(item => mapTree(item))
+    return <TreeView hasAnimations aria-label='Tree View with checkboxes example' data={mapped} onCheck={onCheck} hasCheckboxes />
 }
